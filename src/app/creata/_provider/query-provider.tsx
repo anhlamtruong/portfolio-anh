f7ddf8b276635d07f9ac59802550a09a9f4ad5eb
Anh Lam Truong <85408593+anhlamtruong@users.noreply.github.com>
2025-04-22 00:18:02 -0400

    add to client query

--- src/app/creata/_provider/query-provider.tsx
+++ src/app/creata/_provider/query-provider.tsx
@@ -1,16 +1,48 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/react-query";
 import React from "react";
 
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
 // Create the QueryClient outside the component to persist across renders
-const queryClientCreata = new QueryClient();
+
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 export function CreataQueryProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClientCreata = getQueryClient();
   return (
     <QueryClientProvider client={queryClientCreata}>
       {children}
