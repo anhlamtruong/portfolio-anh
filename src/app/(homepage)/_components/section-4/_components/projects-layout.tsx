88123cae94baad7107d7f9c895d8fa6a4e02fcfe
Lam Anh Truong <emcaemchut@Lams-MacBook-Pro.local>
2025-10-02 16:30:26 -0400

    add details to projects

--- src/app/(homepage)/_components/section-4/_components/projects-layout.tsx
+++ src/app/(homepage)/_components/section-4/_components/projects-layout.tsx
@@ -8,6 +8,7 @@ type Card = {
   content: JSX.Element | React.ReactNode | string;
   className: string;
   thumbnail: string;
+  title: string;
 };
 
 export const ProjectLayoutGrid = ({ cards }: { cards: Card[] }) => {
@@ -25,7 +26,7 @@ export const ProjectLayoutGrid = ({ cards }: { cards: Card[] }) => {
   };
 
   return (
-    <div className="w-full h-full p-10 grid grid-cols-1 md:grid-cols-3  max-w-7xl mx-auto gap-4 relative">
+    <div className="w-full h-full p-10 grid grid-cols-1 md:grid-cols-3   mx-auto gap-4 relative">
       {cards.map((card, i) => (
         <div key={i} className={cn(card.className, "")}>
           <motion.div
@@ -34,7 +35,7 @@ export const ProjectLayoutGrid = ({ cards }: { cards: Card[] }) => {
               card.className,
               "relative overflow-hidden",
               selected?.id === card.id
-                ? "rounded-lg cursor-pointer absolute inset-0 h-1/2 w-full md:w-1/2 m-auto z-50 flex justify-center items-center flex-wrap flex-col"
+                ? " rounded-lg cursor-pointer absolute inset-0 h-5/6 w-11/12 md:w-5/6 m-auto z-50 flex justify-center items-center flex-wrap flex-col"
                 : lastSelected?.id === card.id
                   ? "z-40 bg-white rounded-xl h-full w-full"
                   : "bg-white rounded-xl h-full w-full"
@@ -42,7 +43,10 @@ export const ProjectLayoutGrid = ({ cards }: { cards: Card[] }) => {
             layoutId={`card-${card.id}`}
           >
             {selected?.id === card.id && <SelectedCard selected={selected} />}
-            <ImageComponent card={card} />
+            <ImageComponent
+              className={cn(selected?.id ? "blur-sm  " : "")}
+              card={card}
+            />
           </motion.div>
         </div>
       ))}
@@ -58,24 +62,48 @@ export const ProjectLayoutGrid = ({ cards }: { cards: Card[] }) => {
   );
 };
 
-const ImageComponent = ({ card }: { card: Card }) => {
+const ImageComponent = ({
+  card,
+  className,
+}: {
+  card: Card;
+  className?: string;
+}) => {
   return (
-    <motion.img
-      layoutId={`image-${card.id}-image`}
-      src={card.thumbnail}
-      height="500"
-      width="500"
-      className={cn(
-        "object-cover object-top absolute inset-0 h-full w-full transition duration-200"
-      )}
-      alt="thumbnail"
-    />
+    <div className="group ">
+      <motion.img
+        layoutId={`image-${card.id}-image`}
+        src={card.thumbnail}
+        height="auto"
+        width="auto"
+        className={cn(
+          className,
+          "object-cover object-left absolute inset-0 h-full w-full transition duration-200"
+        )}
+        alt="thumbnail"
+      />
+      {/* Add a dark gradient overlay for text readability */}
+      <div
+        className={cn(
+          className,
+          "absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-transparent"
+        )}
+      />
+
+      {/* The Title with hover animation */}
+      <motion.h3
+        layoutId={`title-${card.id}`}
+        className="z-10 absolute bottom-4 p-5 left-4 text-white text-lg md:text-xl font-semibold transform transition-transform duration-300 ease-in-out group-hover:-translate-y-2 "
+      >
+        {card.title}
+      </motion.h3>
+    </div>
   );
 };
 
 const SelectedCard = ({ selected }: { selected: Card | null }) => {
   return (
-    <div className="bg-transparent h-full w-full flex flex-col justify-end rounded-lg shadow-2xl relative z-[60]">
+    <div className="bg-transparent h-full w-full flex flex-col justify-top shadow-2xl relative z-[60]">
       <motion.div
         initial={{
           opacity: 0,
@@ -83,7 +111,7 @@ const SelectedCard = ({ selected }: { selected: Card | null }) => {
         animate={{
           opacity: 0.6,
         }}
-        className="absolute inset-0 h-full w-full bg-black opacity-60 z-10"
+        className="absolute inset-0 h-full w-full rounded-lg bg-black opacity-95 z-20"
       />
       <motion.div
         layoutId={`content-${selected?.id}`}
@@ -103,7 +131,7 @@ const SelectedCard = ({ selected }: { selected: Card | null }) => {
           duration: 0.3,
           ease: "easeInOut",
         }}
-        className="relative px-8 pb-4 z-[70]"
+        className="relative py-9 px-8 pb-4 z-[70]"
       >
         {selected?.content}
       </motion.div>
